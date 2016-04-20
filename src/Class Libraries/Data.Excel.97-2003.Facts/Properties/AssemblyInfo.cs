4dfdba446c779c10d31c25a36e226320c4f09f8c
adean <alan.dean@gmail.com>
2016-04-20 09:25:41 +0100

    Ported from Google Code Project.

---
+++ src/Class Libraries/Data.Excel.97-2003.Facts/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyDefaultAlias("Cavity.Data.Excel.97-2003.Facts.dll")]
+[assembly: AssemblyTitle("Cavity.Data.Excel.97-2003.Facts.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Excel 97-2003 Data Facts Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Excel 97-2003 Data Facts Library (Release)")]
+
+#endif
---
+++ src/Class Libraries/Data.Excel.97-2003/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+[assembly: CLSCompliant(false)]
+[assembly: AssemblyDefaultAlias("Cavity.Data.Excel.97-2003.dll")]
+[assembly: AssemblyTitle("Cavity.Data.Excel.97-2003.dll")]
+
+#if (DEBUG)
+
+[assembly: AssemblyDescription("Cavity : Excel 97-2003 Data Library (Debug)")]
+
+#else
+
+[assembly: AssemblyDescription("Cavity : Excel 97-2003 Data Library (Release)")]
+
+#endif
